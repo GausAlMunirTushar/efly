cbcbf0bdaa98553c3ef8ceb2b89bf6e54acd105b
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-03-13 00:34:42 +0600

    feat: enhance HomeNavbar with dropdown menus for Visa and Services

--- src/components/layouts/HomeNavbar.tsx
+++ src/components/layouts/HomeNavbar.tsx
@@ -3,8 +3,8 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { ChevronDown, Menu, X } from 'lucide-react'
-import Image from 'next/image'
+import { ChevronRight, Menu, X } from 'lucide-react'
+import { motion } from 'framer-motion'
 
 const navItems = [
 	{ name: 'Home', href: '/' },
@@ -12,22 +12,38 @@ const navItems = [
 	{ name: 'Jobs', href: '/flight' },
 	{ name: 'Events', href: '/hotel' },
 	{ name: 'Holiday', href: '/holiday' },
-	{ name: 'Visa', href: '/visa' }
-]
-
-const moreItems = [
-	{ name: 'Package Deals', href: '/packages' },
-	{ name: 'Travel Guide', href: '/guide' }
+	{
+		name: 'Visa',
+		href: '#',
+		submenu: [
+			{ name: 'Visa on Arrival', href: '/visa-on-arrival' },
+			{ name: 'Visa Free', href: '/visa-free' },
+			{ name: 'Visa Required', href: '/visa-required' },
+			{ name: 'Visa Extension', href: '/visa-extension' }
+		]
+	},
+	{
+		name: 'Services',
+		href: '#',
+		submenu: [
+			{ name: 'Package Deals', href: '/packages' },
+			{ name: 'Travel Guide', href: '/guide' }
+		]
+	}
 ]
 
 export default function HomeNavbar() {
-	const [isDropdownOpen, setDropdownOpen] = useState(false)
 	const [isMenuOpen, setMenuOpen] = useState(false)
-	const pathname = usePathname() // Get current pathname
+	const [activeDropdown, setActiveDropdown] = useState<number | null>(null)
+	const pathname = usePathname()
+
+	// Explicitly type the index parameter as a number
+	const handleMouseEnter = (index: number) => setActiveDropdown(index)
+	const handleMouseLeave = () => setActiveDropdown(null)
 
 	return (
-		<nav className=' bg-white border-b'>
-			<div className='container mx-auto flex justify-between items-center'>
+		<nav className='bg-white shadow'>
+			<div className='container mx-auto flex justify-between items-center py-2.5'>
 				{/* Logo */}
 				<div className='flex items-center space-x-2'>
 					<img
@@ -40,47 +56,59 @@ export default function HomeNavbar() {
 				</div>
 
 				{/* Desktop Navigation */}
-				<div className='hidden md:flex items-center space-x-6 pt-4'>
-					{navItems.map(item => (
-						<Link
+				<div className='hidden md:flex items-center'>
+					{navItems.map((item, index) => (
+						<div
 							key={item.name}
-							href={item.href}
-							className={`relative pb-3 text-gray-800 ${
-								pathname === item.href
-									? 'border-b-4 border-primary text-primary transition-all duration-500'
-									: 'hover:border-b-4 hover:border-primary hover:text-primary transition-all duration-500'
-							}`}
+							className='relative'
+							onMouseEnter={() =>
+								item.submenu && handleMouseEnter(index)
+							}
+							onMouseLeave={handleMouseLeave}
 						>
-							{item.name}
-						</Link>
-					))}
+							<Link
+								href={item.href}
+								className={`relative text-gray-800 font-semibold px-4 py-5 ${
+									pathname === item.href
+										? 'text-primary'
+										: 'hover:bg-primary hover:text-white hover:border-primary transition-all duration-500'
+								}`}
+							>
+								{item.name}
+							</Link>
 
-					{/* Dropdown */}
-					<div className='relative'>
-						<button
-							className='flex text-gray-800 items-center pb-2 hover:text-primary hover:border-b-4 hover:border-primary'
-							onClick={() => setDropdownOpen(!isDropdownOpen)}
-						>
-							More <ChevronDown size={16} className='ml-1' />
-						</button>
-						{isDropdownOpen && (
-							<div className='absolute mt-2 bg-white shadow-lg rounded-md p-2 w-40'>
-								{moreItems.map(item => (
-									<Link
-										key={item.name}
-										href={item.href}
-										className={`block px-4 py-2 hover:bg-gray-100 ${
-											pathname === item.href
-												? 'bg-gray-200'
-												: ''
-										}`}
-									>
-										{item.name}
-									</Link>
-								))}
-							</div>
-						)}
-					</div>
+							{/* Submenu */}
+							{item.submenu && activeDropdown === index && (
+								<motion.div
+									initial={{ opacity: 0, y: -10 }}
+									animate={{ opacity: 1, y: 0 }}
+									exit={{ opacity: 0, y: -10 }}
+									transition={{ duration: 0.3 }}
+									className='absolute mt-5 bg-white shadow min-w-48'
+								>
+									{item.submenu.map(subitem => (
+										<Link
+											key={subitem.name}
+											href={subitem.href}
+											className={`block py-2.5 hover:bg-gray-100 ${
+												pathname === subitem.href
+													? 'bg-gray-200'
+													: 'hover:bg-primary hover:text-white hover:border-primary transition-all duration-500'
+											}`}
+										>
+											<div className='flex px-2 items-center gap-2'>
+												<ChevronRight
+													size={18}
+													className='text-primary hover:text-white'
+												/>{' '}
+												{subitem.name}
+											</div>
+										</Link>
+									))}
+								</motion.div>
+							)}
+						</div>
+					))}
 				</div>
 
 				{/* Action Buttons (Desktop) */}
@@ -107,49 +135,48 @@ export default function HomeNavbar() {
 				{/* Mobile Navigation */}
 				{isMenuOpen && (
 					<div className='absolute top-14 left-0 w-full bg-white shadow-md md:hidden'>
-						<div className='flex flex-col items-start px-4 py-2 space-y-2'>
-							{navItems.map(item => (
-								<Link
-									key={item.name}
-									href={item.href}
-									className={`w-full px-4 py-2 border-l-4 ${
-										pathname === item.href
-											? 'border-primary text-primary'
-											: 'border-transparent hover:border-primary hover:text-primary'
-									}`}
-								>
-									{item.name}
-								</Link>
-							))}
+						<div className='flex flex-col items-start px-4 py-4 space-y-2'>
+							{navItems.map((item, index) => (
+								<div key={item.name} className='w-full'>
+									<Link
+										href={item.href}
+										className={`w-full px-4 py-4  ${
+											pathname === item.href
+												? 'border-primary text-primary'
+												: 'border-transparent hover:border-primary hover:text-primary'
+										}`}
+									>
+										{item.name}
+									</Link>
 
-							{/* Dropdown for Mobile */}
-							<div className='w-full'>
-								<button
-									className='w-full flex justify-between px-4 py-2 border-l-4 border-transparent hover:border-primary hover:text-primary'
-									onClick={() =>
-										setDropdownOpen(!isDropdownOpen)
-									}
-								>
-									More <ChevronDown size={16} />
-								</button>
-								{isDropdownOpen && (
-									<div className='w-full bg-gray-100 rounded-md p-2'>
-										{moreItems.map(item => (
-											<Link
-												key={item.name}
-												href={item.href}
-												className={`block px-4 py-2 hover:bg-gray-200 ${
-													pathname === item.href
-														? 'bg-gray-300'
-														: ''
-												}`}
+									{/* Submenu for Mobile */}
+									{item.submenu &&
+										activeDropdown === index && (
+											<motion.div
+												initial={{ opacity: 0, y: -10 }}
+												animate={{ opacity: 1, y: 0 }}
+												exit={{ opacity: 0, y: -10 }}
+												transition={{ duration: 0.3 }}
+												className='w-full bg-gray-100 rounded-md p-2'
 											>
-												{item.name}
-											</Link>
-										))}
-									</div>
-								)}
-							</div>
+												{item.submenu.map(subitem => (
+													<Link
+														key={subitem.name}
+														href={subitem.href}
+														className={`block px-4 py-4 hover:bg-gray-200 ${
+															pathname ===
+															subitem.href
+																? 'bg-gray-300'
+																: ''
+														}`}
+													>
+														{subitem.name}
+													</Link>
+												))}
+											</motion.div>
+										)}
+								</div>
+							))}
 
 							{/* Action Buttons (Mobile) */}
 							<Link

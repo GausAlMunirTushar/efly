3f5170f4cde9ce476da544cd067d8230ad30caa2
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-03-26 11:24:17 +0600

    feat: integrate BlogSlider into BlogLayout for improved blog presentation

--- src/app/(public)/blog/layout.tsx
+++ src/app/(public)/blog/layout.tsx
@@ -3,11 +3,15 @@
 import { ReactNode } from 'react'
 import BlogNavbar from '@/components/layouts/blog/BlogNavbar'
 import BlogSidebar from '@/components/layouts/blog/BlogSidebar'
+import BlogSlider from '@/components/pages/front-pages/blog/BlogSlider'
 
 export default function BlogLayout({ children }: { children: ReactNode }) {
 	return (
 		<section>
 			<BlogNavbar />
+			<div className='container mx-auto my-4'>
+				<BlogSlider />
+			</div>
 			<div className='container mx-auto flex gap-6 my-2 w-full'>
 				<div className='w-full sm:w-2/12 h-full'>
 					<BlogSidebar />
--- src/app/(public)/blog/page.tsx
+++ src/app/(public)/blog/page.tsx
@@ -1,12 +1,8 @@
 import BlogList from '@/components/pages/front-pages/blog/BlogList'
-import BlogSlider from '@/components/pages/front-pages/blog/BlogSlider'
 
 export default function BlogPage() {
 	return (
 		<section>
-			<div className='mb-4'>
-				<BlogSlider />
-			</div>
 			<BlogList />
 		</section>
 	)

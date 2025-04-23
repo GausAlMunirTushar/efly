7bd66e214edb432dd86b75a719a49327b5921f86
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-04-23 09:57:04 +0600

    feat: add robots.txt and sitemap.ts files for SEO optimization

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	return [
+		{
+			url: 'https://efly.com.bd',
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 1
+		},
+		{
+			url: 'https://efly.com.bd/about',
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 0.8
+		},
+		{
+			url: 'https://efly.com.bd//blog',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 1
+		}
+	]
+}

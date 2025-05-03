4529be54d3ca77cd04f9ffd59c4ea712cd74346d
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-05-03 12:52:40 +0600

    feat: add new airline logos and update airline data references in MostPopularAirlines component

--- src/components/pages/front-pages/flight/MostPopularAirlines.tsx
+++ src/components/pages/front-pages/flight/MostPopularAirlines.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image'
 import { Airline } from '@/types/Airline'
+import { ChevronRight } from 'lucide-react'
 
 interface MostPopularAirlinesProps {
 	airlines: Airline[]
@@ -17,26 +18,29 @@ export default function MostPopularAirlines({
 					Most Popular Airlines
 				</h2>
 				<p className='text-gray-600 mb-10'>
-					Discover top airlines on ShareTrip and seamlessly search any
+					Discover top airlines on efly and seamlessly search any
 					flight and get any online ticket instantly, granting you
 					effortless access to global travel.
 				</p>
-				<div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6'>
+				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4	 gap-6'>
 					{airlines.map((airline, index) => (
 						<div
 							key={index}
-							className='flex items-center gap-3 bg-gray-50 hover:bg-gray-100 transition rounded-md px-4 py-3 cursor-pointer'
+							className='flex justify-between  items-center gap-3 bg-gray-100 hover:bg-gray-200 transition rounded-md px-4 py-3 cursor-pointer'
 						>
-							<Image
-								src={airline.logo}
-								alt={airline.name}
-								width={32}
-								height={32}
-								className='object-contain'
-							/>
-							<span className='text-sm font-medium text-gray-800'>
-								{airline.name}
-							</span>
+							<div className='flex items-center  gap-4 sm:gap-1'>
+								<Image
+									src={airline.logo}
+									alt={airline.name}
+									width={32}
+									height={32}
+									className='object-contain'
+								/>
+								<span className='text-sm font-medium text-gray-800 text-wrap '>
+									{airline.name}
+								</span>
+							</div>
+							<ChevronRight className='w-4 h-4' />
 						</div>
 					))}
 				</div>

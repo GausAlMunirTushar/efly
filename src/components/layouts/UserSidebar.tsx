d04342594702345478f06bfc371fd951d9e6bf95
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-05-03 21:39:39 +0600

    feat: update UserSidebar component to enhance user navigation experience

--- src/components/layouts/UserSidebar.tsx
+++ src/components/layouts/UserSidebar.tsx
@@ -54,14 +54,14 @@ export default function UserSidebar() {
 
 	return (
 		<aside className='w-60 bg-white dark:bg-bg_dark border-r dark:border-bg_secondary border-gray-200 shadow-md flex flex-col rounded-lg overflow-y-auto scrollbar-none'>
-			<nav className='space-y-1 p-2'>
+			<nav className='space-y-1 py-5'>
 				{filteredMenuItems.map(item => (
 					<Link
 						key={item.title}
 						href={item.href!}
-						className={`flex items-center gap-2 p-2 rounded-md transition-all duration-300 hover:bg-primary dark:hover:bg-bg_secondary hover:text-white text-gray-600 dark:text-text-primary font-semibold ${
+						className={`flex items-center gap-2 px-5 py-2.5  transition-all duration-300 hover:text-primary dark:hover:bg-bg_secondary  text-gray-600 dark:text-text-primary font-semibold ${
 							pathname === item.href
-								? 'bg-primary text-white dark:text-text-primary'
+								? 'border-l-4 border-primary bg-blue-100 text-primary'
 								: ''
 						}`}
 					>

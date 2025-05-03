e329e704909f56540c1ab4cc8d2fac4c24c66708
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-05-03 20:58:06 +0600

    feat: add user pages for booking, dashboard, deposit, invoice, my account, my services, settings, and wishlist; update UserSidebar with new routes

--- src/app/(public)/(user)/layout.tsx
+++ src/app/(public)/(user)/layout.tsx
@@ -1,36 +1,19 @@
 'use client'
 
-import HomeNavbar from '@/components/layouts/HomeNavbar'
 import UserSidebar from '@/components/layouts/UserSidebar'
-import { useState } from 'react'
 
 export default function UserDashboardLayout({
 	children
 }: {
 	children: React.ReactNode
 }) {
-	const [isExpanded, setIsExpanded] = useState(false)
-	const [isPersistent, setIsPersistent] = useState(false)
-
-	const toggleSidebar = () => {
-		setIsPersistent(!isPersistent)
-		setIsExpanded(!isPersistent)
-	}
-
 	return (
 		<section className='h-screen bg-gray-100  transition-all duration-300'>
-			{/* Navbar */}
-			<div className='sticky top-0 z-10'>
-				<HomeNavbar />
-			</div>
 			{/* Main Content - No extra spacing issues */}
 			<div className='flex max-w-6xl h-screen container mx-auto transition-all duration-300 bg-gray-100'>
-				<aside className='mt-4 h-screen overflow-y-auto'>
+				<aside className='mt-4 '>
 					{/* Sidebar */}
-					<UserSidebar
-						isExpanded={isExpanded}
-						setIsExpanded={setIsExpanded}
-					/>
+					<UserSidebar />
 				</aside>
 				{/* Scrollable Content */}
 				<main className='flex-1 overflow-y-auto scrollbar-none m-4'>

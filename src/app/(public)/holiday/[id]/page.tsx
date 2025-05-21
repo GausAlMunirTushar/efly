5b27c70536f81416d79b1773b7bdc343d35408e1
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-05-21 15:55:47 +0600

    feat: add error handling for holiday package fetching in HolidayDetailsPage

--- src/app/(public)/holiday/[id]/page.tsx
+++ src/app/(public)/holiday/[id]/page.tsx
@@ -12,9 +12,12 @@ interface HolidayDetailsPageProps {
 
 export async function generateMetadata({
 	params
-}: HolidayDetailsPageProps): Promise<Metadata> {
+}: {
+	params: Promise<{ id: string }>
+}): Promise<Metadata> {
+	const { id } = await params
 	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_APP_URL}/api/holiday/${params.id}`,
+		`${process.env.NEXT_PUBLIC_APP_URL}/api/holiday/${id}`,
 		{ cache: 'no-store' }
 	)
 	if (!res.ok) return {}
@@ -29,9 +32,12 @@ export async function generateMetadata({
 
 export default async function HolidayDetailsPage({
 	params
-}: HolidayDetailsPageProps) {
+}: {
+	params: Promise<{ id: string }>
+}) {
+	const { id } = await params
 	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_APP_URL}/api/holiday/${params.id}`,
+		`${process.env.NEXT_PUBLIC_APP_URL}/api/holiday/${id}`,
 		{ cache: 'no-store' }
 	)
 

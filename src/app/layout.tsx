bd28153a8a797eaec627766e99ec72bf115d03cb
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-05-21 16:02:12 +0600

    feat: add custom font integration and update global styles in layout and globals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,20 @@
 import '@/styles/globals.scss'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { Open_Sans, Poppins } from 'next/font/google'
 
 import { ToastContextProvider } from '@/contexts/ToastContextProvider'
+
+const openSans = Open_Sans({
+	subsets: ['latin'],
+	variable: '--font-open-sans',
+	weight: ['400', '500', '600', '700']
+})
+
+const poppins = Poppins({
+	subsets: ['latin'],
+	variable: '--font-poppins',
+	weight: ['400', '500', '600', '700']
+})
 
 export const metadata = {
 	title: 'efly - We Are The New Travel Agency (OTA) in Your City',
@@ -15,7 +28,7 @@ interface LayoutProps {
 
 export default async function RootLayout({ children }: LayoutProps) {
 	return (
-		<html>
+		<html className={`${openSans.variable} ${poppins.variable}`}>
 			<head></head>
 			<body>
 				<ToastContextProvider>{children}</ToastContextProvider>

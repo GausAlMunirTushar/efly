eadb59bb69b53e0f44f687599bac29d97ad5ba56
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-07-05 14:04:08 +0600

    fix: adjust layout of VisaDetailPage for better responsiveness

--- src/app/(public)/visa/[id]/page.tsx
+++ src/app/(public)/visa/[id]/page.tsx
@@ -51,7 +51,7 @@ export default function VisaDetailPage() {
 					imageUrl={visa.country.image}
 				/>
 
-				<div className='w-full flex flex-wrap gap-6 py-6'>
+				<div className='w-full flex gap-6 py-6'>
 					<div className='w-full sm:w-9/12'>
 						<h1 className='text-3xl font-bold mb-4'>
 							{visa.country.name} Visa
